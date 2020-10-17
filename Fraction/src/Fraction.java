6d3fd0a6f16b6e71756d03ca8d790c7f767d009c
Skywalker409 <71032363+Skywalker409@users.noreply.github.com>
2020-10-16 21:32:31 -0400

    Add files via upload

---
+++ Fraction/src/Fraction.java
@@ -0,0 +1,33 @@
+import java.lang.Math;
+public class Fraction {
+	
+	private int num;
+	private int dem;
+	public double posDiff(double a, double b) {
+		return (a-b);
+		
+	}
+	 public double xpower(double a, double b) {
+			//Math.pow
+				double power = Math.pow(a,b);
+				
+				return power;
+				
+			}
+	 public double add(double a, double b) {
+			return (a+b);
+	 
+	 }
+	 public double multi(double a, double b) {
+			return (a*b);
+	 }
+	 public double div(double a, double b) {
+			return (a/b);
+	 }
+	 
+	}
+	
+	
+	
+	
+
